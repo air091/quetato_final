b596a8924f123d3223a044472d2636e88fc34bbc
air051 <acesparkle243@gmail.com>
2026-06-23 21:51:12 +0800

    register controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,3 +1,43 @@
+const registerController = async (request, response) => {
+  try {
+    const { username, email, password } = request.body;
+    const agent = request.headers["user-agent"] || "Unknown Device";
+    const ipAddress = request.ip || "127.0.0.1";
+
+    const tokens = await register({
+      username,
+      email,
+      password,
+      ipAddress,
+      agent,
+    });
+
+    response.cookie("session", tokens.refresh, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      path: "/",
+    });
+
+    return response.status(200).json({ success: true, tokens });
+  } catch (error) {
+    console.error("Register failed", error);
+
+    let statusCode = 500;
+    let errorMessage = "Internal Server Error";
+
+    if (error instanceof AppError) {
+      statusCode = error.statusCode;
+      errorMessage = error.message;
+    }
+
+    return response
+      .status(statusCode)
+      .json({ success: false, error: errorMessage });
+  }
+};
+
 const loginController = async (request, response) => {
   try {
     const { email, password } = request.body;
@@ -10,7 +50,7 @@ const loginController = async (request, response) => {
       { status: 200 },
     );
 
-    response.cookie("token", tokens.refresh, {
+    response.cookie("session", tokens.refresh, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: "strict",
