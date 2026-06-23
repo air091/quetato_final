ca42cca43ed28b5d84565b8f34201599fc322d9a
air051 <acesparkle243@gmail.com>
2026-06-23 23:16:14 +0800

    new table community

---
+++ prisma/migrations/20260623151542_community_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Community" (
+    "id" TEXT NOT NULL,
+    "ownerId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Community_pkey" PRIMARY KEY ("id")
+);
